7a346a3df4a2425adfcf853937534ec283f58bcd
AriLopezM <ian.lopez31i@std.uni.edu.ni>
2024-05-12 20:53:54 -0600

    Add project files.

---
+++ ConsoleApp6/Automovil.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    public class programa
+    {
+        static void Main(string[] args)
+        {
+            Automovil carro = new Automovil
+            {
+                Marca = "McLaren",
+                Placa = "XYZ123",
+                Modelo = "Formula1",
+                Color = "Negro",
+                CapacidadTanqueCombustible = 15,
+                RendimientoCombustible = 30
+            };
+
+            // Intenta conducir 100 km
+            if (carro.PuedeConducir(100))
+            {
+                carro.Conducir(100);
+            }
+            else
+            {
+                Console.WriteLine("No es posible conducir la distancia especificada con el combustible actual.");
+                // Reabastecer antes de conducir
+                carro.ReabastecerCombustible(10);
+                carro.Conducir(100);
+            }
+        }
+    }
+}
---
+++ ConsoleApp6/Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    public partial class Automovil
+    {
+        public string Marca { get; set; }
+        public string Placa { get; set; }
+        public string Modelo { get; set; }
+        public string Color { get; set; }
+        public double CapacidadTanqueCombustible { get; set; }
+        public double NivelCombustible { get; set; } = 0;
+        public double RendimientoCombustible { get; set; }
+
+        public void Conducir(int distancia)
+        {
+            double combustibleNecesario = distancia / RendimientoCombustible;
+            if (NivelCombustible >= combustibleNecesario)
+            {
+                NivelCombustible -= combustibleNecesario;
+                Console.WriteLine($"El carro ha conducido {distancia} km y le queda {NivelCombustible} galones de combustible.");
+            }
+            else
+            {
+                Console.WriteLine("No hay suficiente combustible para realizar el viaje.");
+            }
+        }
+
+        public void ReabastecerCombustible(double cantidad)
+        {
+            if (cantidad + NivelCombustible <= CapacidadTanqueCombustible)
+            {
+                NivelCombustible += cantidad;
+                Console.WriteLine($"Se ha reabastecido el tanque. Nivel de combustible actual: {NivelCombustible} galones.");
+            }
+            else
+            {
+                Console.WriteLine("La cantidad excede la capacidad máxima del tanque.");
+            }
+        }
+
+        public bool PuedeConducir(int distancia)
+        {
+            return (distancia / RendimientoCombustible) <= NivelCombustible;
+        }
+    }
+}
